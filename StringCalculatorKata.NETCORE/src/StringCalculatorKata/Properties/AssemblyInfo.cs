57fe6f2b10847db5b46768b9d76e912e64bb1ed7
Brittany Berwanger <bberwanger@expl.com>
2016-08-05 15:49:44 -0500

    Added .NET Core example project

---
+++ StringCalculatorKata.NETCORE/src/StringCalculatorKata/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringCalculatorKata")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5785df91-a56d-421b-a935-d3fff4f7ce89")]
---
+++ StringCalculatorKata.NETCORE/test/StringCalculatorKata.UnitTests/StringCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculatorKata.UnitTests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Add_WhenGivenEmptyString_ReturnsZero()
+        {
+            arrangeActAndAssertExpectedEqualsActual(default(string), 0);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("200", 200)]
+        public void Add_WhenGivenSingleNumber_ReturnsThatNumber(string value, int expectedResult)
+        {
+            arrangeActAndAssertExpectedEqualsActual(value, expectedResult);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("1,2,3", 6)]
+        [TestCase("1,2,3,4", 10)]
+        public void Add_WhenGivenCommaDelimitedNumbers_ReturnsTheSumOfThoseNumbers(string value, int expectedResult)
+        {
+            arrangeActAndAssertExpectedEqualsActual(value, expectedResult);
+        }
+
+        [Test]
+        public void Add_WhenGivenNegativeNumber_ThrowsException()
+        {
+            // Arrange (Given)
+            var stringCalculator = new StringCalculator();
+
+            // Act (When)
+            TestDelegate testDelegate = delegate { stringCalculator.Add("-1, 2"); };
+
+            // Assert (Then)
+            Assert.Throws<Exception>(testDelegate);
+        }
+
+        private static void arrangeActAndAssertExpectedEqualsActual(string value, int expectedResult)
+        {
+            // Arrange (Given)
+            var stringCalculator = new StringCalculator();
+
+            // Act (When)
+            int actualResult = stringCalculator.Add(value);
+
+            // Assert (Then)
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
